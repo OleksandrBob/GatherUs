3a8db36f275738e8bf66d2b5ecce5dace4b20c0e
Oleksandr Bob <o.bob@reply.io>
2024-05-24 19:51:04 +0300

    Added new categories

--- GatherUs.Enums/CustomEventCategory.cs
+++ GatherUs.Enums/CustomEventCategory.cs
@@ -8,11 +8,25 @@ public enum CustomEventCategory
     PopMusic = 3,
     RapMusic = 4,
     IndieMusic = 5,
-    
+
     //medicine
     Pulmonology = 6,
     Cardiology = 7,
     Pediatrics = 8,
-    Surgery=9,
-    Dentistry=10,
+    Surgery = 9,
+    Dentistry = 10,
+
+    //sport
+    Football = 11,
+    Basketball = 12,
+    Hockey = 13,
+    Tennis = 14,
+    Rugby = 15,
+
+    //books
+    Fiction = 16,
+    Science = 17,
+    Horror = 18,
+    Romance = 19,
+    Comics = 20,
 }
